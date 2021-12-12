ef0ead5fe605967a39691eb6dbcd7489fc68af94
RomanKovalenko61 <astek14@mail.ru>
2021-12-12 17:06:51 +0300

    Course: Spring for beginners - add scr and pom

---
+++ src/main/java/com/zaurtregulov/spring/rest/App.java
@@ -0,0 +1,29 @@
+package com.zaurtregulov.spring.rest;
+
+import com.zaurtregulov.spring.rest.configuration.MyConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+
+//        List<Employee> allEmployees = communication.getAllEmployees();
+//        System.out.println(allEmployees);
+
+//        Employee empByID = communication.getEmployee(1);
+//        System.out.println(empByID);
+
+//        Employee emp = new Employee("Sveta", "Sokolova", "IT", 1200);
+//        emp.setId(10);
+//        communication.saveEmployee(emp);
+
+        communication.deleteEmployee(10);
+
+        context.close();
+    }
+}
